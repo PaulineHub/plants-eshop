cfb20d1b3506f67b1c38ffbaef9b17244e5afe83
Pauline <huby.pauline@gmail.com>
2022-09-09 17:44:40 -0400

    reorganisation dossier public

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,47 @@
 const Product = require('../models/product');
 
-const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({category:'plantes'})
+// Get all products
+const getAllProducts = async (req,res) => {
+    const {featured, category, name, sort, fields} = req.query;
+    const queryObject = {};
+
+    if(featured){
+        queryObject.featured = featured ==='true'? true : false
+    } 
+    if(category){
+        queryObject.category = category
+    }
+    if(name){
+        queryObject.name = {$regex:name, $options:'i'}
+    }
+
+    let result = Product.find(queryObject);
+
+   //SORT
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList)
+    } else {
+        result = result.sort('createAt')
+    }
+
+    // SELECT
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList)
+    }
+
+    // PAGINATION
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
     res.status(200).json({products})
 }
 
-const getAllProducts = async (req,res) => {
-    const products = await Product.find(req.query)
-    res.status(200).json({products, nbHits: products.length})
-}
-
 module.exports = {
-    getAllProductsStatic,
     getAllProducts
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllProductsStatic, getAllProducts} = require('../controllers/products');
+const {getAllProducts} = require('../controllers/products');
 
 router.route('/').get(getAllProducts);
-router.route('/static').get(getAllProductsStatic);
 
 module.exports = router

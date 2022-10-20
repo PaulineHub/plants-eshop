81b1f929e7a560a129206472f2789116a4475311
Pauline <huby.pauline@gmail.com>
2022-10-20 11:21:13 -0400

    reorganisation fichiers

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import { useState } from 'react';
+import SharedLayout from './pages/SharedLayout'
+import Home from './pages/Home';
+// import About from './pages/About';
+// import Products from './pages/Products';
+// import Error from './pages/Error';
+
+// import SingleProduct from './pages/SingleProduct';
+// import SharedProductLayout from './pages/SharedProductLayout';
+
+function App() {
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       <Route path='/' element={<SharedLayout />}>
+  //         <Route index element={<Home />} />
+          
+  //         <Route path='about' element={<About />} />
+
+  //         <Route path='products' element={<SharedProductLayout />}>
+  //           <Route index element={<Products />} />
+  //           <Route path=':productId' element={<SingleProduct />} />
+  //         </Route>
+
+  //         <Route path='*' element={<Error />} />
+  //       </Route>
+  //     </Routes>
+  //     <footer>footer</footer>
+  //   </BrowserRouter>
+  // )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ src/pages/SharedLayout.js
@@ -0,0 +1,11 @@
+import { Link, Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
+export default Home;

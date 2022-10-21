3be0d59f2a713e277aac1eeee7170be5ab8fbf7f
Pauline <huby.pauline@gmail.com>
2022-10-21 15:10:13 -0400

    ajout toggle navbar mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png'
+import React, { useState } from 'react'
 
 const Navbar = () => {
-  return (
+    const [isActive, setActive] = useState(false)
+    const displayMobileNav = () => {
+        setActive(!isActive)
+    }
+    return (
     <nav>
         <Link to="./">
             <div className="logo-container">
                 <img src={logo} alt=""/>
             </div>
         </Link>
-        <div className="menu">
+        <div className={isActive ? "menu show-nav" : 'menu'}>
             <ul className="menuPrincipal">
                 <Link to="./products">
                     <li className="menuPrincipalLi dark-color">Nos Produits</li>
@@ -20,7 +25,7 @@ const Navbar = () => {
             </ul>
         </div>
         <div className="fas-wrapper">
-            <i className="fas fa-bars dark-color"></i>
+            <i className="fas fa-bars dark-color" onClick={displayMobileNav}></i>
             <div className="shop-icon-ctn">
                 <i className="fas fa-shopping-bag dark-color"></i>
                 <div className="circle"></div>

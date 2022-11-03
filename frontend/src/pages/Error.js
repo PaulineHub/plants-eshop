133f85f07f7c2ffdc36c51ae4a93dc292caf9e2b
Pauline <huby.pauline@gmail.com>
2022-11-03 10:16:00 -0400

    Ajout page error 404

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,8 +1,12 @@
+
 const Error = () => {
   return (
-    <section className='section'>
-      <h2>Error page</h2>
-    </section>
-  );
+    <main className="error-background">
+      <div>
+        <h1>404</h1>
+        <h4>Erreur! Nous n'avons pas trouvé votre page.</h4>
+      </div>
+    </main>
+  )
 };
 export default Error;
